9cea9cc0e6cce07f3f22f4ad49e198b05e1b67d2
black-RAM <pplanes311@gmail.com>
2024-04-08 23:17:08 +0300

    production build

--- src/main.jsx
+++ src/main.jsx
@@ -1,20 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import rawMarkup from "./sample.md?raw"
 import App from './App.jsx'
-
-async function fetchSample() {
-  try {
-    const response = await fetch('/sample.txt')
-    if (!response.ok) throw new Error('Failed to fetch Markdown content')
-    const markdownText = await response.text()
-    return markdownText
-  } catch (error) {
-    console.error('Error fetching sample content:', error);
-  }
-}
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App sample={await fetchSample()}/>
-  </React.StrictMode>,
+    <App sample={rawMarkup}/>
+  </React.StrictMode>
 )
+
